2a51b72da83d146f170c986efbcea563b2a97a43
maggie8 <329391692@qq.com>
2018-11-14 08:28:46 +0800

    day1014

---
+++ test14.php
@@ -0,0 +1,14 @@
+<?php
+
+function Add($num1,$num2)
+{
+    while($num2!=0){
+        $temp = $num1^$num2;
+        $num2 = ($num1 & $num2)<<1;
+        $num1=$temp;
+    }
+    return $num1;
+
+}
+$sum = Add(100,10);
+echo $sum;
